0b368bf3b4160a4ba8ade5912a960604ab344be2
tuanhm9898 <107852574+tuanhm9898@users.noreply.github.com>
2022-08-23 18:37:16 +0700

    login vd thay Tham

--- BE/BE_eHealthCare/src/main/java/edu/multicampus/eHealthCare/config/WebSecurityConfig.java
+++ BE/BE_eHealthCare/src/main/java/edu/multicampus/eHealthCare/config/WebSecurityConfig.java
@@ -1,17 +1,65 @@
 package edu.multicampus.eHealthCare.config;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import edu.multicampus.eHealthCare.security.jwt.AuthEntryPointJwt;
+import edu.multicampus.eHealthCare.security.jwt.AuthTokenFilter;
+import edu.multicampus.eHealthCare.security.services.UserDetailsServiceImpl;
+
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		// securedEnabled = true,
+		// jsr250Enabled = true,
+		prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
 	}
 
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/test/**").permitAll().anyRequest()
+				.authenticated();
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+}
 //	@Bean
 //	CorsConfigurationSource corsConfigurationSource() {
 //		CorsConfiguration configuration = new CorsConfiguration();
@@ -23,4 +71,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //		source.registerCorsConfiguration("/**", configuration);
 //		return source;
 //	}
-}
+
---
+++ BE/BE_eHealthCare/src/main/java/edu/multicampus/eHealthCare/model/ERole.java
@@ -0,0 +1,7 @@
+package edu.multicampus.eHealthCare.model;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
