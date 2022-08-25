d2afb6d6bae9554e9a19ac799929a52ce3d375f4
ngoducdo <ngo2682@gmail.com>
2022-08-25 10:40:09 +0700

    update

--- FE/front-end/src/routesPatient.js
+++ FE/front-end/src/routesPatient.js
@@ -5,6 +5,13 @@ import History from "views/Patient/History.js";
 
 const dashboardRoutes = [
   {
+    path: "/",
+    name: "Thông tin cá nhân",
+    icon: "nc-icon nc-circle-09",
+    component: Profile,
+    layout: "/patient"
+  },
+  {
     path: "/booking",
     name: "Đặt lịch khám",
     icon: "nc-icon nc-tap-01",
@@ -23,13 +30,6 @@ const dashboardRoutes = [
     name: "Lịch sử khám bệnh",
     icon: "nc-icon nc-notes",
     component:History,
-    layout: "/patient"
-  },
-  {
-    path: "/",
-    name: "Thông tin cá nhân",
-    icon: "nc-icon nc-circle-09",
-    component: Profile,
     layout: "/patient"
   },
 ];
