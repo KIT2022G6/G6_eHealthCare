a240e2cb8f07479810ce341ee6d24c3c78bc4d6d
ngoducdo <ngo2682@gmail.com>
2022-08-26 09:01:59 +0700

    update

--- FE/front-end/src/views/Patient/History.js
+++ FE/front-end/src/views/Patient/History.js
@@ -54,10 +54,10 @@ const History = () => {
     if (currentItems != null) {
         listappointment = currentItems.map((item, id) => (
             <tr>
-                <td>{item.id}</td>
                 <td>{item.appointmentID}</td>
-                <td>{item.pRoo}</td>
-                <td>{item.pVsi}</td>
+                <td>{item.ordinalNum}</td>
+                <td>{item.room}</td>
+                <td>{item.visitDate}</td>
                 {/* <td>{item.price}</td>
                 <td>{item.category}</td>
                 <td>{item.details_shorts}</td>
@@ -124,7 +124,7 @@ const History = () => {
                 <thead>
                     <tr>
                         <th>STT</th>
-                        <th>Mã phòng khám</th>
+                        <th>Số người đợi</th>
                         <th>Số phòng</th>
                         <th>Ngày khám</th>
                     </tr>
